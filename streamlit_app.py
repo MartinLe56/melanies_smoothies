4a625f13e0be0c5b0338fb7c06d8a193e6dbf438
MartinLe56 <martin_dt56@yahoo.de>
2025-04-10 13:55:36 +0200

    Update streamlit_app.py
    Put the API Output into a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,9 +72,11 @@ if ingredients_list: #if List is NULL
         #Confirmation that smoothie is ordered
         st.success('Your Smoothie is ordered!', icon="✅")
 
+#Setup API Requests
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
